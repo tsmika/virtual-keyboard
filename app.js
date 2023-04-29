f453879e3405fd19d8d94b79ceb19354f7486c83
tsmika <tsikhankaras@gmail.com>
2023-04-29 16:11:02 +0300

    feat: add backspace functionality

--- app.js
+++ app.js
@@ -14,61 +14,61 @@ let engKeySet = {
   Equal: "=",
   Backspace: "Backspace",
   Tab: "Tab", // second line
-  q: "KeyQ",
-  w: "KeyW",
-  e: "KeyE",
-  r: "KeyR",
-  t: "KeyT",
-  y: "KeyY",
-  u: "KeyU",
-  i: "KeyI",
-  o: "KeyO",
-  p: "KeyP",
-  BracketLeft: "[",
-  BracketRight: "]",
-  Backslash: "\\",
-  Delete: "Del",
-  CapsLock: "CapsLock", // third line
-  a: "KeyA",
-  s: "KeyS",
-  d: "KeyD",
-  f: "KeyF",
-  g: "KeyG",
-  h: "KeyH",
-  j: "KeyJ",
-  k: "KeyK",
-  l: "KeyL",
-  Semicolon: ":",
-  Quote: "\"",
-  Enter: "Enter",
-  ShiftLeft: "Shift", // fourth line
-  IntlBackslash: "\\",
-  z: "KeyZ",
-  x: "KeyX",
-  c: "KeyC",
-  v: "KeyV",
-  b: "KeyB",
-  n: "KeyN",
-  m: "KeyM",
-  Comma: ",",
-  Period: ".",
-  Slash: "/",
-  ArrowUp: "▲",
-  ShiftRight: "ShiftRight",
-  ShiftLeft: "ShiftLeft",
-  ControlLeft: "Ctrl", // fifth line
-  MetaLeft: "Win",
-  AltLeft: "Alt",
-  Space: "Space",
-  AltRight: "Alt",
-  ControlRight: "Ctrl",
-  ArrowLeft: "◄",
-  ArrowDown: "▼",
-  ArrowRight: "►",
+  KeyQ: 'q',
+  KeyW: 'w',
+  KeyE: 'e',
+  KeyR: 'r',
+  KeyT: 't',
+  KeyY: 'y',
+  KeyU: 'u',
+  KeyI: 'i',
+  KeyO: 'o',
+  KeyP: 'p',
+  BracketLeft: '[',
+  BracketRight: ']',
+  Backslash: '\\',
+  Delete: 'Delete',
+  CapsLock: 'CapsLock', // third line
+  KeyA: 'a',
+  KeyS: 's',
+  KeyD: 'd',
+  KeyF: 'f',
+  KeyG: 'g',
+  KeyH: 'h',
+  KeyJ: 'j',
+  KeyK: 'k',
+  KeyL: 'l',
+  Semicolon: ";",
+  Quote: '\'',
+  Enter: 'Enter',
+  ShiftLeft: 'Shift', // fourth line
+  IntlBackslash: '\\',
+  KeyZ: 'z',
+  KeyX: 'x',
+  KeyC: 'c',
+  KeyV: 'v',
+  KeyB: 'b',
+  KeyN: 'n',
+  KeyM: 'm',
+  Comma: ',',
+  Period: '.',
+  Slash: '/',
+  ArrowUp: 'ArrowUp',
+  ShiftRight: 'Shift',
+  ControlLeft: 'Control', // fifth line
+  MetaLeft: 'Meta',
+  AltLeft: 'Alt',
+  Space: 'Space',
+  AltRight: 'Alt',
+  ControlRight: 'Control',
+  ArrowLeft: 'ArrowLeft',
+  ArrowDown: "ArrowDown",
+  ArrowRight: "ArrowRight",
 };
 
 document.onkeydown = function (event) {
-  engKeyboard.push(event.Code + ' : ' + event.key);
+  // engKeyboard.push(event.Code + ' : ' + event.key);
+  console.log(event.code + ' : ' + event.key);
 }
 
 let engKeyboard = [];
@@ -78,9 +78,9 @@ document.body.append(main);
 
 let screen = document.createElement('textarea');
 screen.className = 'screen';
-screen.onkeydown = function (event) {
-  engKeySet[event.key] = event.code;
-}
+// screen.onkeydown = function (event) {
+//   engKeySet[event.key] = event.code;
+// }
 
 main.append(screen);
 
@@ -97,22 +97,45 @@ for (item in engKeySet) {
     engKeyboardString += '<div class="keyboard-row">';
   }
 
-  title = Object.values(engKeySet)[i];
+  title = Object.keys(engKeySet)[i];
 
   if (title[0] === 'K') title = title.at(3);
+  if (title[0] + title[1] === 'Di') title = title.at(5);
 
   switch (title) {
-    case 'Tab':
-    case 'Ctrl': keySizeClass = ' middle-key'; break;
-    case ('ShiftLeft'):
-    case 'Enter': keySizeClass = ' double-key'; break;
-    case 'CapsLock':
-    case 'Backspace': keySizeClass = ' long-key'; break;
+    case 'Backquote': title = '\`'; break;
     case 'Space': keySizeClass = ' huge-key'; break;
+    case 'Backspace': keySizeClass = ' long-key'; break;
+    case 'CapsLock': title = 'CapsLock'; keySizeClass = ' long-key'; break;
+    case 'ControlLeft': title = 'Ctrl'; keySizeClass = ' middle-key'; break;
+    case 'Tab': title = 'Tab'; keySizeClass = ' middle-key'; break;
+    case 'Enter': keySizeClass = ' double-key'; break;
+    case 'ShiftLeft': title = 'Shift'; keySizeClass = ' double-key'; break;
+    case 'Equal': title = '='; break;
+    case 'Backslash': title = '\\'; break;
+    case 'Minus': title = '-'; break;
+    case 'Delete': title = 'Del'; break;
+    case 'BracketLeft': title = '['; break;
+    case 'BracketRight': title = ']'; break;
+    case 'Semicolon': title = ';'; break;
+    case 'Quote': title = '\''; break;
+    case 'IntlBackslash': title = '\\'; keySizeClass = ''; break;
+    case 'Comma': title = ','; break;
+    case 'Period': title = '.'; break;
+    case 'Slash': title = '/'; break;
+    case 'ArrowUp': title = '▲'; break;
+    case 'ShiftRight': title = 'Shift'; break;
+    case 'MetaLeft': title = 'Win'; break;
+    case 'AltLeft': title = 'Alt'; break;
+    case 'AltRight': title = 'Alt';
+    case 'ControlRight':  title = 'Ctrl'; keySizeClass = ''; break;
+    case 'ArrowLeft': title = '◄'; break;
+    case 'ArrowDown': title = '▼'; break;
+    case 'ArrowRight': title = '►'; break;
     default: keySizeClass = '';
   }
 
-  if (title[0] + title[1] === 'Sh') title = 'Shift';
+  // if (title[0] + title[1] === 'Sh') title = 'Shift';
 
   engKeyboardString += '<div class="key' + keySizeClass + '">' + title + '</div>';
 
@@ -126,3 +149,34 @@ for (item in engKeySet) {
 keyboard.innerHTML = engKeyboardString;
 
 main.append(keyboard);
+
+let key = document.querySelectorAll('.key');
+let scr = document.querySelector('.screen');
+let clear = document.querySelector('.clear');
+let content = '';
+
+for (let k of key) {
+  k.onclick = function () {
+    content = k.textContent;
+    console.log(content);
+    let screenContent = scr.textContent;
+    console.log("screenContent:" + screenContent);
+    if (k.textContent === 'Backspace') {
+      scr.textContent = screenContent.slice(0, screenContent.length - 1);
+      console.log("screenContent.slice")
+    } else {
+      scr.textContent = screenContent + k.textContent;
+    }
+  }
+}
+
+
+// document.onkeydown = function(event){
+//   document.querySelector('keyboard .key')
+//   document.ge
+// }
+
+// clear.onclick = function(evt){
+//   evt.preventDefault();
+//   scr.textContent = '';
+// }
