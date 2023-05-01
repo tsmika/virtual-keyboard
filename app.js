4bbc439a85d956a37321e770b22a29fca5f58e63
tsikhankaras <tsikhankaras@gmail.com>
2023-05-02 00:13:28 +0300

    feat: add rus languge

--- app.js
+++ app.js
@@ -66,6 +66,74 @@ let engKeySet = {
   ArrowRight: "ArrowRight",
 };
 
+let rusKeySet = {
+  Backquote: "ё", // first line
+  Digit1: "1",
+  Digit2: "2",
+  Digit3: "3",
+  Digit4: "4",
+  Digit5: "5",
+  Digit6: "6",
+  Digit7: "7",
+  Digit8: "8",
+  Digit9: "9",
+  Digit0: "0",
+  Minus: "-",
+  Equal: "=",
+  Backspace: "Backspace",
+  Tab: "Tab", // second line
+  KeyQ: 'й',
+  KeyW: 'ц',
+  KeyE: 'у',
+  KeyR: 'к',
+  KeyT: 'е',
+  KeyY: 'н',
+  KeyU: 'г',
+  KeyI: 'ш',
+  KeyO: 'щ',
+  KeyP: 'з',
+  BracketLeft: 'х',
+  BracketRight: 'ъ',
+  Backslash: '\\',
+  Delete: 'Delete',
+  CapsLock: 'CapsLock', // third line
+  KeyA: 'ф',
+  KeyS: 'ы',
+  KeyD: 'в',
+  KeyF: 'а',
+  KeyG: 'п',
+  KeyH: 'р',
+  KeyJ: 'о',
+  KeyK: 'л',
+  KeyL: 'д',
+  Semicolon: "ж",
+  Quote: 'э',
+  Enter: 'Enter',
+  ShiftLeft: 'Shift', // fourth line
+  IntlBackslash: '\\',
+  KeyZ: 'я',
+  KeyX: 'ч',
+  KeyC: 'с',
+  KeyV: 'м',
+  KeyB: 'и',
+  KeyN: 'т',
+  KeyM: 'ь',
+  Comma: 'б',
+  Period: 'ю',
+  Slash: '.',
+  ArrowUp: 'ArrowUp',
+  ShiftRight: 'Shift',
+  ControlLeft: 'Control', // fifth line
+  MetaLeft: 'Meta',
+  AltLeft: 'Alt',
+  Space: 'Space',
+  AltRight: 'Alt',
+  ControlRight: 'Control',
+  ArrowLeft: 'ArrowLeft',
+  ArrowDown: "ArrowDown",
+  ArrowRight: "ArrowRight",
+};
+
 let engKeyboard = [];
 let main = document.createElement('main');
 main.className = 'main';
@@ -78,65 +146,73 @@ main.append(screen);
 
 let keyboard = document.createElement('div');
 keyboard.className = 'keyboard';
-let engKeyboardString = '';
+let keyboardString = '';
 let i = 0;
 let title = '';
+let key = '';
 let keySizeClass = '';
+let lang = 'eng';
 
-for (item in engKeySet) {
-  if (i === 0 || i === 14 || i === 29 || i === 42 || i === 56) {
-    engKeyboardString += '<div class="keyboard-row">';
+let keySet = (lang === 'eng') ? engKeySet : rusKeySet;
+initKeySet(keySet);
+
+function initKeySet(keySet) {
+
+  for (item in keySet) {
+    if (i === 0 || i === 14 || i === 29 || i === 42 || i === 56) {
+      keyboardString += '<div class="keyboard-row">';
+    }
+
+    title = Object.values(keySet)[i];
+    key = Object.keys(keySet)[i];
+
+    if (title[0] === 'K') title = title.at(3).toLowerCase();
+    if (title[0] + title[1] === 'Di') title = title.at(5);
+
+    switch (key) {
+      case 'Backquote': (keySet === 'eng') ? title = '\`' : 'ё'; break;
+      case 'Backspace': keySizeClass = ' long-key'; break;
+      case 'CapsLock': title = 'CapsLock'; keySizeClass = ' long-key'; break;
+      case 'ControlLeft': title = 'Ctrl'; keySizeClass = ' middle-key'; break;
+      case 'Tab': title = 'Tab'; keySizeClass = ' middle-key'; break;
+      case 'Enter': keySizeClass = ' double-key'; break;
+      case 'ShiftLeft': title = 'Shift'; keySizeClass = ' long-key'; break;
+      case 'Equal': title = '='; break;
+      case 'Backslash': title = '\\'; break;
+      case 'Minus': title = '-'; break;
+      case 'Delete': title = 'Del'; break;
+      case 'BracketLeft': (keySet === 'eng') ? title = '[' : 'х'; break;
+      case 'BracketRight': (keySet === 'eng') ? title = ']' : 'ъ'; break;
+      case 'Semicolon': (keySet === 'eng') ? title = ';' : 'ж'; break;
+      case 'Quote': (keySet === 'eng') ? title = '\'' : 'э'; break;
+      case 'IntlBackslash': title = '\\'; keySizeClass = ''; break;
+      case 'Comma': (keySet === 'eng') ? title = ',' : 'б'; break;
+      case 'Period': (keySet === 'eng') ? title = '.' : 'ю'; break;
+      case 'Slash': title = '/'; break;
+      case 'ArrowUp': title = '▲'; break;
+      case 'ShiftRight': title = 'Shift'; break;
+      case 'MetaLeft': title = 'Win'; break;
+      case 'AltLeft': title = 'Alt'; keySizeClass = ''; break;
+      case 'Space': keySizeClass = ' huge-key'; break;
+      case 'AltRight': title = 'Alt'; keySizeClass = ''; break;
+      case 'ControlRight': title = 'Ctrl'; keySizeClass = ' middle-key'; break;
+      case 'ArrowLeft': title = '◄'; keySizeClass = ''; break;
+      case 'ArrowDown': title = '▼'; keySizeClass = ''; break;
+      case 'ArrowRight': title = '►'; keySizeClass = ''; break;
+      default: keySizeClass = ''; break;
+    }
+
+    keyboardString += '<div class="key' + keySizeClass + '" data="' + item + '">' + title + '</div>';
+
+    if (i === 13 || i === 28 || i === 41 || i === 55 || i === 64) {
+      keyboardString += '</div>';
+    }
+
+    i++;
   }
-
-  title = Object.keys(engKeySet)[i];
-
-  
-  if (title[0] === 'K') title = title.at(3).toLowerCase();
-  if (title[0] + title[1] === 'Di') title = title.at(5);
-
-  switch (title) {
-    case 'Backquote': title = '\`'; break;
-    case 'Backspace': keySizeClass = ' long-key'; break;
-    case 'CapsLock': title = 'CapsLock'; keySizeClass = ' long-key'; break;
-    case 'ControlLeft': title = 'Ctrl'; keySizeClass = ' middle-key'; break;
-    case 'Tab': title = 'Tab'; keySizeClass = ' middle-key'; break;
-    case 'Enter': keySizeClass = ' double-key'; break;
-    case 'ShiftLeft': title = 'Shift'; keySizeClass = ' double-key'; break;
-    case 'Equal': title = '='; break;
-    case 'Backslash': title = '\\'; break;
-    case 'Minus': title = '-'; break;
-    case 'Delete': title = 'Del'; break;
-    case 'BracketLeft': title = '['; break;
-    case 'BracketRight': title = ']'; break;
-    case 'Semicolon': title = ';'; break;
-    case 'Quote': title = '\''; break;
-    case 'IntlBackslash': title = '\\'; keySizeClass = ''; break;
-    case 'Comma': title = ','; break;
-    case 'Period': title = '.'; break;
-    case 'Slash': title = '/'; break;
-    case 'ArrowUp': title = '▲'; break;
-    case 'ShiftRight': title = 'Shift'; break;
-    case 'MetaLeft': title = 'Win'; break;
-    case 'AltLeft': title = 'Alt'; keySizeClass = ''; break;
-    case 'Space': keySizeClass = ' huge-key'; break;
-    case 'AltRight': title = 'Alt'; keySizeClass = ''; break;
-    case 'ControlRight': title = 'Ctrl'; keySizeClass = ' middle-key'; break;
-    case 'ArrowLeft': title = '◄'; keySizeClass = ''; break;
-    case 'ArrowDown': title = '▼'; keySizeClass = ''; break;
-    case 'ArrowRight': title = '►'; keySizeClass = ''; break;
-    default: keySizeClass = ''; break;
-  }
-
-  engKeyboardString += '<div class="key' + keySizeClass + '" data="' + item + '">' + title + '</div>';
-
-  if (i === 13 || i === 28 || i === 41 || i === 55 || i === 64) {
-    engKeyboardString += '</div>';
-  }
-
-  i++;
 }
 
-keyboard.innerHTML = engKeyboardString;
+keyboard.innerHTML = keyboardString;
 
 main.append(keyboard);
 
@@ -170,25 +246,20 @@ for (let k of keys) {
       scr.textContent = screenContent.slice(0, scr.selectionStart) + k.textContent + screenContent.slice(scr.selectionStart, screenContent.length);
       scr.selectionStart = cursorPosition + 1;
     }
-
-    console.log("globalContent: " + document.querySelector('.screen').textContent);
-
   }
 }
+scr.focus();
 
-document.onkeydown = function (event) {
-
-
-  document.querySelectorAll('.key').forEach(function (element) {
-    element.classList.remove('pressed');
-  });
-
-  document.querySelector('.key[data="' + event.code + '"]').classList.add('pressed');
-  setTimeout(removePressing, 100);
-  function removePressing() {
-    document.querySelector('.key[data="' + event.code + '"]').classList.remove('pressed');
+  document.onkeydown = function (event) {
+    document.querySelectorAll('.key').forEach(function (element) {
+      element.classList.remove('pressed');
+    });
+    document.querySelector('.key[data="' + event.code + '"]').classList.add('pressed');
   }
-}
+
+ document.onkeyup = function (event) {
+   document.querySelector('.key[data="' + event.code + '"]').classList.remove('pressed');
+ }
 
 function getCaretPos() {
   scr.focus();
@@ -202,7 +273,6 @@ function getCaretPos() {
   } else if (scr.selectionStart !== false) return scr.selectionStart;
   else return 0;
 }
-
 
 let info = document.createElement('div');
 info.innerHTML = '<h2 style="display: block; width:35%; margin: auto;  margin-top: 2%;">Клавиатура создана в операционной системе Windows</h2>';
